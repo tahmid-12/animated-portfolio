3cc98d4f4b814eb24d5027eee1369a29f249ced1
Md. Tahmid Chowhdhury <mdtahmidchowdhury12@gmail.com>
2025-12-15 00:27:13 +0600

    CV Added

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
-import { PiHexagonThin } from "react-icons/pi";
+// import { PiHexagonThin } from "react-icons/pi";
 
 const HeroPic = () => {
   return (
@@ -11,15 +11,17 @@ const HeroPic = () => {
       viewport={{ once: false, amount: 0 }}
       className=" h-full flex items-center justify-center "
     >
+      {/* Tahmid-removebg-preview.png */}
       <img
         src="/images/HexaPic.png"
+        // src="/images/Tahmid-removebg-preview.png"
         alt="Abdelrahman Qassem"
         className="max-h-[450px] w-auto"
       />
 
-      <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
+      {/* <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
-      </div>
+      </div> */}
     </motion.div>
   );
 };
